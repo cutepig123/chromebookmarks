df71f59d641c3ca45bfec05f3e6f7f71c6d7cd97
jshe <jinshouhe@gmail.com>
2021-02-27 18:36:03 +0800

    initial commit

---
+++ 3-chromebookmarks.bat
@@ -0,0 +1,4 @@
+set PYTHONIOENCODING=utf-8
+path %path%;C:\Python36\Python36
+python.exe %~dp0%\testChromeFirefoxBookmarks.py >3-bookmarks.md
+pause
---
+++ test_chrome_bookmarks.py
@@ -0,0 +1,5 @@
+import chrome_bookmarks
+
+for url in chrome_bookmarks.urls:
+    print(url.name, url.url, url)
+    
